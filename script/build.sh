8e4185192fa56d957f865eb523f1dafd3e2cf6de
Szilard Ledan <szledan@inf.u-szeged.hu>
2019-12-15 21:49:31 +0100

    Init CelMech a simple celestial mechanics program

---
+++ script/build.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+mkdir -p build
+
+g++ -o build/celmech src/*
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/* CelMech
+ * Copyright (C) 2019, Szilard Ledan <szledan@gmail.com>
+ * All rights reserved.
+ *
+ * APACHE LICENSE, VERSION 2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
+ */
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
